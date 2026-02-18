58e97d1c74686f8de4f6c33289a1ad6ed6fba5f2
Aryan Palaspagar <118288039+i-am-ap@users.noreply.github.com>
2026-02-18 16:44:46 +0530

    update

--- api/weather.js
+++ api/weather.js
@@ -1,12 +1,25 @@
+import 'dotenv/config';
+
 export default async function handler(req, res){
+
+try{
 
 const city = req.query.city || "Mumbai";
 
-const response = await fetch(
+const r = await fetch(
 `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_KEY}&q=${city}&days=7&aqi=yes&alerts=yes`
 );
 
-const data = await response.json();
+if(!r.ok){
+return res.status(500).json({error:"Weather API failed"});
+}
+
+const data = await r.json();
 
 res.status(200).json(data);
+
+}catch(err){
+res.status(500).json({error:err.message});
+}
+
 }
